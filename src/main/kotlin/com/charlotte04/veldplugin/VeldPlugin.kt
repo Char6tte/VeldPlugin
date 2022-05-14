7c454cc51be915de9311f5092d9636d4cb760409
BuildTools <unconfigured@null.spigotmc.org>
2022-05-15 00:23:10 +0900

    fix plugin.yml fix README ・add command "veld"

--- src/main/kotlin/com/charlotte04/veldplugin/VeldPlugin.kt
+++ src/main/kotlin/com/charlotte04/veldplugin/VeldPlugin.kt
@@ -3,19 +3,6 @@ package com.charlotte04.veldplugin
 import com.charlotte04.veldplugin.commands.VeldCommandHandler
 import org.bukkit.command.CommandExecutor
 import org.bukkit.plugin.java.JavaPlugin
-import org.bukkit.plugin.java.annotation.command.Command
-import org.bukkit.plugin.java.annotation.command.Commands
-import org.bukkit.plugin.java.annotation.plugin.ApiVersion
-import org.bukkit.plugin.java.annotation.plugin.Plugin
-import org.bukkit.plugin.java.annotation.plugin.author.Author
-
-
-@Plugin(name = "VeldPlugin", version = "0.1")
-@ApiVersion(ApiVersion.Target.v1_18)
-@Author("Char6tte")
-@Commands(
-    Command(name = "veld")
-)
 
 class VeldPlugin : JavaPlugin(){
 
@@ -23,6 +10,8 @@ class VeldPlugin : JavaPlugin(){
     override fun onEnable() {
         // Plugin startup logical
         server.consoleSender.sendMessage("[Veld]Enabled")
+
+        //commands startup
         regCommand("veld", VeldCommandHandler)
     }
 
@@ -34,7 +23,7 @@ class VeldPlugin : JavaPlugin(){
 
     }
 
-    private fun regCommand(command:String, executor:CommandExecutor){
-        getCommand(command)?.setExecutor(executor)
+    private fun regCommand(name :String, executor:CommandExecutor){
+        getCommand(name)?.setExecutor(executor)
     }
 }
