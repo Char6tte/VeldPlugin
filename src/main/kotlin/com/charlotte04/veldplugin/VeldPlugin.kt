8dce2fa8a51950e1264fefe94001b6f0ee3b1be4
BuildTools <unconfigured@null.spigotmc.org>
2022-05-15 20:10:10 +0900

    "ve" command to return message

--- src/main/kotlin/com/charlotte04/veldplugin/VeldPlugin.kt
+++ src/main/kotlin/com/charlotte04/veldplugin/VeldPlugin.kt
@@ -2,15 +2,16 @@ package com.charlotte04.veldplugin
 
 import com.charlotte04.veldplugin.commands.VeldCommandHandler
 import org.bukkit.ChatColor
+import org.bukkit.ChatColor.AQUA
+import org.bukkit.ChatColor.GREEN
 import org.bukkit.command.CommandExecutor
 import org.bukkit.plugin.java.JavaPlugin
 
 open class VeldPlugin : JavaPlugin(){
 
-
     override fun onEnable() {
         // Plugin startup logical
-        server.consoleSender.sendMessage("[Veld]Enabled")
+        consoleMes("Enabled", GREEN)
 
         //commands startup
         regCommand("veld", VeldCommandHandler)
@@ -24,14 +25,17 @@ open class VeldPlugin : JavaPlugin(){
 
     }
 
-    fun consoleMes(string: String, color: ChatColor){
-        this.server.consoleSender.sendMessage("[VariousCore]$color$string")
-    }
+
 
     private fun regCommand(name: String, executor: CommandExecutor) {
         getCommand(name)?.run {
             setExecutor(executor)
-            consoleMes("/$name を登録しました", ChatColor.AQUA)
+            consoleMes("/$name を登録しました", AQUA)
         } ?: logger.severe("/$name を登録できませんでした")
     }
+}
+
+fun VeldPlugin.consoleMes(string: String, color: ChatColor){
+    this.server.consoleSender.sendMessage("[VeldPlugin]$color$string")
+
 }
--- src/main/kotlin/com/charlotte04/veldplugin/commands/VeldCommandHandler.kt
+++ src/main/kotlin/com/charlotte04/veldplugin/commands/VeldCommandHandler.kt
@@ -1,17 +1,16 @@
 package com.charlotte04.veldplugin.commands
 
-import com.charlotte04.veldplugin.VeldPlugin
-import org.bukkit.ChatColor.AQUA
-import org.bukkit.ChatColor.RED
 import org.bukkit.command.Command
 import org.bukkit.command.CommandExecutor
 import org.bukkit.command.CommandSender
 
-object VeldCommandHandler : CommandExecutor , VeldPlugin() {
+object VeldCommandHandler : CommandExecutor{
+
+
     override fun onCommand(sender: CommandSender, command: Command, label: String, args: Array<out String>): Boolean {
-            consoleMes("=======VeldPlugin=======",RED)
-            consoleMes("/ve setting",AQUA)
-            consoleMes("========================",RED)
+        sender.server.consoleSender.sendMessage("======== ========")
+        sender.server.consoleSender.sendMessage("/veld")
+        sender.server.consoleSender.sendMessage("======== ========")
         return true
     }
 
