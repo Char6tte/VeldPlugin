0993b27a2ded07eeff98278a0d0c85584ff2fee3
BuildTools <unconfigured@null.spigotmc.org>
2022-05-15 15:57:35 +0900

    "ve" command to return message

--- src/main/kotlin/com/charlotte04/veldplugin/commands/VeldCommandHandler.kt
+++ src/main/kotlin/com/charlotte04/veldplugin/commands/VeldCommandHandler.kt
@@ -1,7 +1,6 @@
 package com.charlotte04.veldplugin.commands
 
 import com.charlotte04.veldplugin.VeldPlugin
-import com.charlotte04.veldplugin.util.LoadPanels
 import org.bukkit.ChatColor.AQUA
 import org.bukkit.ChatColor.RED
 import org.bukkit.command.Command
@@ -10,22 +9,9 @@ import org.bukkit.command.CommandSender
 
 object VeldCommandHandler : CommandExecutor , VeldPlugin() {
     override fun onCommand(sender: CommandSender, command: Command, label: String, args: Array<out String>): Boolean {
-        if (args.isNotEmpty()) {
-            when (args[0]) {
-                "setting" -> {
-                }
-                "menu" -> {
-                    LoadPanels()
-                }
-                else -> {
-
-                }
-            }
-        }else{
             consoleMes("=======VeldPlugin=======",RED)
             consoleMes("/ve setting",AQUA)
             consoleMes("========================",RED)
-        }
         return true
     }
 
