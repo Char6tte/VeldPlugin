c6f0627fe95c296c8e0b1da44a10a0e250442697
BuildTools <unconfigured@null.spigotmc.org>
2022-05-18 21:29:20 +0900

    "ve" command to return message

--- src/main/kotlin/com/charlotte04/veldplugin/VeldPlugin.kt
+++ src/main/kotlin/com/charlotte04/veldplugin/VeldPlugin.kt
@@ -7,19 +7,14 @@ import org.bukkit.ChatColor
 import org.bukkit.ChatColor.AQUA
 import org.bukkit.ChatColor.GREEN
 import org.bukkit.command.CommandExecutor
-import org.bukkit.plugin.Plugin
 import org.bukkit.plugin.java.JavaPlugin
 
 
 @Suppress("unused")
 open class VeldPlugin : JavaPlugin(){
 
-    companion object : JavaPlugin(){
-        lateinit var plugin: Plugin
-    }
-
     override fun onEnable() {
-        plugin = this
+        //plugin = this
 
         //consoleMes( "Path:" +  getPath().path.toString(), GOLD)
 
